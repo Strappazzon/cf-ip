b3e877d4e3cb9f95733b75cf66514803f968ce71
Alberto Strappazzon <a@strappazzon.xyz>
2021-07-29 20:52:09 +0200

    Refactoring

--- src/ip.js
+++ src/ip.js
@@ -1,20 +1,17 @@
 addEventListener('fetch', e => {
 	const M = e.request.method;
-
 	(M !== 'GET') ? e.respondWith(jsonError(`Method not allowed: ${M}`, 405)) : e.respondWith(handleRequest(e.request));
 });
 
 async function handleRequest(r) {
-	const HEADERS = {
-		'Content-Type'           : 'text/plain; charset=utf-8',
-		'Cache-Control'          : 'no-cache, no-store, must-revalidate',
-		'X-Content-Type-Options' : 'nosniff',
-		'X-Source-Code'          : 'https://github.com/Strappazzon/cf-ip'
-	};
-
 	return new Response(r.headers.get('CF-Connecting-IP') + '\n', {
 		status: 200,
-		headers: HEADERS
+		headers: {
+			'Content-Type'           : 'text/plain; charset=utf-8',
+			'Cache-Control'          : 'no-cache, no-store, must-revalidate',
+			'X-Content-Type-Options' : 'nosniff',
+			'X-Source-Code'          : 'https://github.com/Strappazzon/cf-ip'
+		}
 	});
 }
 
